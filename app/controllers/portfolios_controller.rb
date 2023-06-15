6e88beaf65fb6882dff76f39d3c0e3278941fb52
Rokiyar <maythune654@gmail.com>
2023-06-15 14:22:00 +0630

    show action added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,6 +33,10 @@ class PortfoliosController < ApplicationController
           end
         end
     end
+
+    def show
+      @portfolio_item =  Portfolio.find(params[:id])
+    end
     
     
 
