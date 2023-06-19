99a9d0e9e8432f5dfafec78125363e27c914f2e5
Rokiyar <maythune654@gmail.com>
2023-06-19 14:16:34 +0630

    completed virtual attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first #split will chop the string to array
+  end
+  
+  def last_name
+    self.name.split.last
+  end
 end
