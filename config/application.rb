5d57cd2b2994affcbe5235465683cc3b7a3be094
Rokiyar <maythune654@gmail.com>
2023-06-19 16:24:02 +0630

    added session tracker for dynamic source link

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module RailsPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.middleware.use ActionDispatch::Session::CookieStore
+
 
     # Configuration for the application, engines, and railties goes here.
     #
